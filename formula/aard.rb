9612881a4b2135145ec889922f326eb6435d0705
1xyz <58613330+1xyz@users.noreply.github.com>
2021-04-21 16:30:13 -0700

    v0.9.2 now is aard

--- formula/aard.rb
+++ formula/aard.rb
@@ -4,14 +4,14 @@ require_relative "lib/private_strategy"
 class Aard < Formula
   desc "aard is Aardvark's CLI"
   homepage "https://aardvarklabs.com/"
-  url "https://github.com/aardlabs/homebrew-cli/releases/download/v0.9.1/pruney-darwin-amd64-v0.9.1.zip"
-  sha256 "651cb48b8d611969dc49fc25d497eba144c621bf121d53544d5f2239b393072a"
+  url "https://github.com/aardlabs/homebrew-cli/releases/download/v0.9.2/aard-darwin-amd64-v0.9.2.zip"
+  sha256 "dd2c096a5e654d5f9edfa667124b5b8cb43416172c7e6fb2d3dfebaf27c81f01"
   head "https://github.com/aardlabs/homebrew-cli.git"
   depends_on "coreutils"
 
   def install
     libexec.install Dir["*"]
-    bin.install_symlink libexec/"pruney"
+    bin.install_symlink libexec/"aard"
   end
 
   def caveats
@@ -24,6 +24,6 @@ class Aard < Formula
 
   # Homebrew requires tests.
   test do
-    assert_match "0.91", shell_output("#{bin}/pruney --version", 2)
+    assert_match "0.9.2", shell_output("#{bin}/aard --version", 2)
   end
 end
