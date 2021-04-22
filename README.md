a2c0df53ebf195c7a8df947dc75b6fcb4037eae8
1xyz <58613330+1xyz@users.noreply.github.com>
2021-04-22 15:43:51 -0700

    Templatizing this stuff

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Brew tap for installing the CLI
 $ brew tap aardlabs/cli
 $ brew install aard
 ```
+
+Other
+
+```
+# Update RELEASE_VERSION in script and run
+$ scripts/generate.sh
+
+```
---
+++ scripts/generate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export RELEASE_VERSION=0.9.3
+
+echo "Download https://github.com/aardlabs/homebrew-cli/releases/download/v$RELEASE_VERSION/aard-darwin-amd64-v$RELEASE_VERSION.zip"
+wget https://github.com/aardlabs/homebrew-cli/releases/download/v$RELEASE_VERSION/aard-darwin-amd64-v$RELEASE_VERSION.zip -P /tmp/
+
+stat /tmp/aard-darwin-amd64-v$RELEASE_VERSION.zip
+
+export SHA256_CHECKSUM=$(sha256sum /tmp/aard-darwin-amd64-v$RELEASE_VERSION.zip | cut -d ' ' -f 1)
+
+envsubst < tmpl/aard_rb.tmpl > formula/aard.rb
+
+unset RELEASE_VERSION
+unset SHA256_CHECKSUM
