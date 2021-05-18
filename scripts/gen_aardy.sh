ff7cc6309018874114589135f26036678d5ab69a
1xyz <58613330+1xyz@users.noreply.github.com>
2021-05-18 13:25:16 -0700

    Rename aard to aardy

---
+++ scripts/gen_aardy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z "$RELEASE_VERSION" ]
+then
+      echo "\$RELEASE_VERSION is not set"
+      exit 1
+fi
+
+echo "Download https://github.com/aardlabs/cli-release/releases/download/$RELEASE_VERSION/aard-darwin-amd64-$RELEASE_VERSION.zip"
+wget https://github.com/aardlabs/cli-release/releases/download/$RELEASE_VERSION/aardy-darwin-amd64-$RELEASE_VERSION.zip -P /tmp/
+
+stat /tmp/aardy-darwin-amd64-$RELEASE_VERSION.zip
+
+export SHA256_CHECKSUM=$(sha256sum /tmp/aardy-darwin-amd64-$RELEASE_VERSION.zip | cut -d ' ' -f 1)
+echo "sha256sum $SHA256_CHECKSUM"
+
+envsubst < tmpl/aardy_rb.tmpl > formula/aard.rb
+
+unset RELEASE_VERSION
+unset SHA256_CHECKSUM
